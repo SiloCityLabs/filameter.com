f0d66b895d0c8c02858301f1eb49241409b7abc5
Bana0615 <aelbaneh0615@gmail.com>
2025-03-18 15:40:22 +0000

    [ISSUE-24] PWA: working on getting manage-filament working

--- workbox-config.js
+++ workbox-config.js
@@ -1,23 +1,3 @@
-const loggingPlugin = {
-  fetchDidSucceed: async ({ request, response }) => {
-    console.log("SW: Fetch succeeded:", request.url);
-    return response;
-  },
-  fetchDidFail: async ({ request, error }) => {
-    console.error("SW: Fetch failed:", request.url, error);
-  },
-  cacheDidUpdate: async ({ cacheName, request, response }) => {
-    if (cacheName === "manage-filament-cache") {
-      caches.open(cacheName).then((cache) => {
-        cache.keys().then((keys) => {
-          console.log("SW: Cache Keys:", keys);
-        });
-      });
-    }
-    return response;
-  },
-};
-
 module.exports = {
   globDirectory: "out",
   globPatterns: ["**/*.{js,css,html}", "!manage-filament.html"],
@@ -28,12 +8,9 @@ module.exports = {
       handler: "NetworkFirst",
       options: {
         cacheName: "manage-filament-cache",
-        plugins: [
-          new (require("workbox-expiration").ExpirationPlugin)({
-            maxEntries: 50,
-          }),
-          loggingPlugin, // Apply the logging plugin here
-        ],
+        expiration: {
+          maxEntries: 50,
+        },
       },
     },
   ],

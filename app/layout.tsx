ebd6f9c68acaf9d54f15e520dfdec5fef1f02826
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-09 21:36:59 -0400

    Updated layout file for filameter specific stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import GoogleAnalytics from "@/components/_silabs/GoogleAnalytics";
+import { DatabaseProvider } from "@/contexts/DatabaseContext"; // Import DatabaseProvider
+import ServiceWorkerRegistrar from "@/components/ServiceWorkerRegistrar";
 
 // --- Styles ---
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -50,19 +52,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="main-container">
-          <Header />
-          <main className="main-content">
-            {children} {/* Page content */}
-          </main>
-          <Footer />
-        </div>
+        <DatabaseProvider>
+          <div className="main-container">
+            <Header />
+            <main className="main-content">
+              {children} {/* Page content */}
+            </main>
+            <Footer />
+          </div>
+        </DatabaseProvider>
 
         {GA_TRACKING_ID && (
           <Suspense fallback={null}>
             <GoogleAnalytics gaId={GA_TRACKING_ID} />
           </Suspense>
         )}
+
+        <ServiceWorkerRegistrar />
       </body>
     </html>
   );

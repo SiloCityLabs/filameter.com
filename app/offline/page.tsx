661aa6da789acede7d82f2e84ab3003a79f1d0f2
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-09 22:09:15 -0400

    offline page conversion

---
+++ app/offline/page.tsx
@@ -0,0 +1,32 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Offline",
+};
+
+export default function OfflinePage() {
+  return (
+    <Container className="mt-3 mb-3">
+      <Row className="shadow-lg p-3 bg-body rounded">
+        <Col>
+          <h2 className="text-center">You are offline</h2>
+
+          <div className="text-center">
+            <p>
+              It seems you are not connected to the internet. Please check your
+              connection and try again.
+            </p>
+            <Button
+              onClick={() => window.location.reload()}
+              variant="primary"
+              size="sm"
+            >
+              Retry
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}

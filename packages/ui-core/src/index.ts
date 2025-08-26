1304a4640adadfaafba2e557730e376cabf6104d
Bana0516 <aelbaneh0615@gmail.com>
2025-08-26 09:37:51 -0400

    [Issue-30] Delete SclPlaceholder

--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -7,7 +7,6 @@ export { default as GoogleAnalytics } from './components/GoogleAnalytics';
 export * from './components/LoadingLetters';
 export { default as SclBadge } from './components/SclBadge';
 export { default as SclCard } from './components/SclCard';
-export { default as SclPlaceholder } from './components/SclPlaceholder';
 
 // Export Types
 export type { sclSettings } from './types/settings';
